397581d1d239db2c005cbdd76ba891512fcd78c3
abhi.jeet <zereftemp@gmail.com>
2025-04-27 14:42:31 +0530

    about us page updated

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 import { Routes, Route, BrowserRouter } from "react-router";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import Home from "./components/Home";
-import Header from "./components/Header";
-import Body from "./components/Body"
-import AddNotes from "./components/AddNotes";
-import About from "./components/About";
+import Login from "./src/components/Login";
+import Signup from "./src/components/Signup";
+import Home from "./src/components/Home";
+import Header from "./src/components/Header";
+import Body from "./src/components/Body"
+import AddNotes from "./src/components/AddNotes";
+import About from "./src/components/About";
 import { useState } from "react";
-import StudyMaterialHome from "./components/StudyMaterialHome";
+import StudyMaterialHome from "./src/components/StudyMaterialHome";
 import { useState } from "react";
 
 
@@ -18,9 +18,9 @@ import { useState } from "react";
 const App = () => {
   const [notes, setNotes] = useState([]);
   return (
+    <>
     <BrowserRouter>
     <div>
-
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/signup" element={<Signup />} />
@@ -31,7 +31,7 @@ const App = () => {
       </Routes>
     </div> 
     </BrowserRouter>
-    // <Items/>
+    </>
   );
 };
 
